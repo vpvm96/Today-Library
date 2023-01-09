6d856efa123aa1fae2083c35016d0bc4506c9580
Hoesu Ji <newasborn@naver.com>
2023-01-09 13:55:35 +0900

    feat. 메인, 마이페이지 헤더 탭바 UI 작업

--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -2,19 +2,21 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import styled from '@emotion/native'
 import Mypage from '../screen/Mypage'
 import Home from '../screen/Home'
-import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons, Ionicons } from '@expo/vector-icons'
+// import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons'
+import { Image, TouchableOpacity, View, Text } from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
-const Tabs = () => {
+const Tabs = ({ ProfilePage }) => {
   return (
     <Tab.Navigator
       initialRouteName="Home" // 처음 랜더링시 Home 화면 보여줌
       screenOptions={{
         tabBarShowLabel: false, // 탭바 타이틀 지우기
         headerTitleAlign: 'left', // 안드로이드와 ios 헤더위치가 달라서 left로 값 지정
-        title: '(아이콘 넣을 예정) 오늘의 서재',
+        title: <LogoImage source={require('../assets/images/Hlogo.png')} />,
         tabBarStyle: {
           backgroundColor: '#61D2BC',
         },
@@ -35,6 +37,11 @@ const Tabs = () => {
           tabBarIcon: ({ color }) => (
             <FontAwesome name="user" size={32} color={color} />
           ),
+          headerRight: () => (
+            <SettingIcon>
+              <Ionicons name="ios-settings-sharp" size={20} color="black" />
+            </SettingIcon>
+          ),
         }}
         name="Mypage"
         component={Mypage}
@@ -46,5 +53,9 @@ const Tabs = () => {
 export default Tabs
 
 const LogoImage = styled.Image`
-  width: 20px;
+  width: 120px;
+  height: 30px;
+`
+const SettingIcon = styled.TouchableOpacity`
+  margin-right: 20px;
 `
