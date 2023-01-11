8b7b66b4136af0c0635cda0bb34b720fdbb54f5f
suwoncityBoy <dlgkrrud00@naver.com>
2023-01-11 16:42:55 +0900

    feat : react-query 적용

--- screen/Home.jsx
+++ screen/Home.jsx
@@ -1,31 +1,60 @@
 import React, { useEffect, useState } from 'react'
-import { Alert, View, Text, TouchableOpacity, FlatList } from 'react-native'
+import {
+  Alert,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native'
 import styled from '@emotion/native'
 import { Feather } from '@expo/vector-icons'
-import { getBookRequest } from '../api/bookService'
+import { getMainBookRequest } from '../api/mainBookService'
 import NewBookItem from '../components/MainBookItems/NewBookItem'
 import RecommendBookItem from '../components/MainBookItems/RecommendBookItem'
 import { useQuery, useQueryClient, useInfiniteQuery } from 'react-query'
 
 const Home = () => {
-  const [books, setBooks] = useState([])
-  const [recommendBooks, setRecommendBooks] = useState([])
+  // const [books, setBooks] = useState([])
+  // const [recommendBooks, setRecommendBooks] = useState([])
+  const [isRefreshing, setIsRefreshing] = useState(false)
   const [text, onChangeText] = useState('')
   const [category, setCategory] = useState('newbook')
 
-  // query
   const queryClinet = useQueryClient()
-  const { data, isLoading } = useQuery(getBookRequest())
 
-  const randomBooksfunc = () => {
-    const randomBooks = []
+  // useQeury
+  const {
+    data: bookRequestData,
+    isLoading: isLoadingBR,
+    refetch: refetchBR,
+  } = useQuery('BookRequest', getMainBookRequest)
 
-    for (let i = 0; i < 5; i++) {
-      let randomNum = Math.floor(Math.random() * books.length)
-      randomBooks.push(books[randomNum])
-    }
-    setRecommendBooks(randomBooks)
+  const onRefresh = async () => {
+    setIsRefreshing(true)
+    await refetchBR()
+    setIsRefreshing(false)
   }
+
+  const isLoading = isLoadingBR
+
+  if (isLoading) {
+    return (
+      <StyleLoader>
+        <ActivityIndicator />
+      </StyleLoader>
+    )
+  }
+
+  // const randomBooksfunc = () => {
+  //   const randomBooks = []
+
+  //   for (let i = 0; i < 5; i++) {
+  //     let randomNum = Math.floor(Math.random() * books.length)
+  //     randomBooks.push(books[randomNum])
+  //   }
+  //   setRecommendBooks(randomBooks)
+  // }
 
   // title키워드 검색 시 title에 맞는 db 정보가 불러와 줘야함
   const onSubmitHandler = () => {
@@ -71,13 +100,17 @@ const Home = () => {
       {/* flatlist 영역 */}
       {category === 'newbook' ? (
         <FlatList
-          data={books}
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          data={bookRequestData}
           renderItem={({ item }) => <NewBookItem book={item} />}
           keyExtractor={(item) => item.id}
         />
       ) : (
         <FlatList
-          data={recommendBooks}
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          data={bookRequestData.results}
           renderItem={({ item }) => <RecommendBookItem recbook={item} />}
           keyExtractor={(item) => item.id}
         />
@@ -139,6 +172,12 @@ const StyleCategoryRightButtons = styled.TouchableOpacity`
 const StyleCategoryText = styled.Text`
   font-size: 20px;
   font-weight: 700;
+`
+
+const StyleLoader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `
 
 export default Home
